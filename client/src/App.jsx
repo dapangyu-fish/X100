931e983251c1326794713b045fc1437973d2e2ad
dapangyu <dapangyu@dapangyudeMBP.lan>
2023-08-13 22:32:13 +0800

    init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,14 @@
-// import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
 
 const App = () => (
   <div className="min-h-screen">
-    <h1 className="text-3xl font-blold underline">
-      Hello Fish!
-    </h1>
-    {/* <div className="gradient-bg-welcome">
+    <div className="gradient-bg-welcome">
       <Navbar />
       <Welcome />
     </div>
     <Services />
     <Transactions />
-    <Footer /> */}
+    <Footer />
   </div>
 );
 
