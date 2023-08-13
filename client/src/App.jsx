0cc08712439c4427b20fa971afe3c11fc95a0d66
dapangyu <dapangyu@dapangyudeMBP.lan>
2023-08-13 22:13:11 +0800

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+// import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
+
+const App = () => (
+  <div className="min-h-screen">
+    <h1 className="text-3xl font-blold underline">
+      Hello Fish!
+    </h1>
+    {/* <div className="gradient-bg-welcome">
+      <Navbar />
+      <Welcome />
+    </div>
+    <Services />
+    <Transactions />
+    <Footer /> */}
+  </div>
+);
+
+export default App;
