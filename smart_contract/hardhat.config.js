efbabe106710bb5c0a71678926f22478434e6c7b
dapangyu <dapangyu@dapangyudeMBP.lan>
2023-08-16 21:50:39 +0800

    test

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-mainnet.g.alchemy.com/v2/2dnIYwIHdLiWIZUTaocY2OjB0PpQJrHs
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('@nomiclabs/hardhat-waffle');
+
 module.exports = {
-  solidity: "0.8.19",
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'http://192.168.111.211:8545',
+      accounts: ['c091272d027fbd0b8716c64f4d8ba630967f58a4e1e6b7e61c567dd6e71af990'],
+    },
+  },
 };
