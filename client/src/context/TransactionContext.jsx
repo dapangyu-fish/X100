7d17da359f3898ba321c2242b6603239fd123d8b
dapangyu <dapangyu@dapangyudeMBP.lan>
2023-08-20 19:48:57 +0800

    add func chec connected wallet

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -21,6 +21,17 @@ const getEthereumContract = () => {
 }
 
 export const TransactionProvider = ({ children }) => {
+
+    const checkIfWalletIsConnected = async () =>{
+        if(!ethereum) return alert("Please insall metamask");
+        const accounts = await ethereum.request({method:'eth_accounts'});
+        console.log(accounts);
+    }
+
+    useEffect(() =>{
+        checkIfWalletIsConnected();
+    },[]);
+
     return (
         <TransactionContext.Provider value={{value:'test',}}>
             {children}
