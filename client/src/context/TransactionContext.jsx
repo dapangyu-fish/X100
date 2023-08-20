d31681c5b7d51f232e2dbb44bcfc88b7dcea6cf5
dapangyu <dapangyu@dapangyudeMBP.lan>
2023-08-20 20:28:16 +0800

    连接帐户是隐藏connect wallet 按钮

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -24,11 +24,21 @@ export const TransactionProvider = ({ children }) => {
     const [currentAccount, setCurrentAccount] = useState("");
 
     const checkIfWalletIsConnected = async () =>{
-        if(!ethereum) return alert("Please insall metamask");
+        try{
+            if(!ethereum) return alert("Please insall metamask");
 
-        const accounts = await ethereum.request({method:'eth_accounts'});
-
-        console.log(accounts);
+            const accounts = await ethereum.request({method:'eth_accounts'});
+        
+            if (accounts.length) {
+                setCurrentAccount(accounts[0]);
+                //getAllTransaction():
+            } else {
+                console.log('No accounts found');
+            }
+        }catch (error) {
+            console.log(error);
+            throw new Error("No ethereum object!") 
+        }
     }
 
     const connectWallet = async () =>{
@@ -48,7 +58,7 @@ export const TransactionProvider = ({ children }) => {
     },[]);
 
     return (
-        <TransactionContext.Provider value={{connectWallet}}>
+        <TransactionContext.Provider value={{connectWallet, currentAccount}}>
             {children}
         </TransactionContext.Provider>
     );
